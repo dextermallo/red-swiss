b9c38e8b078f4ad1fcbc855287b85ab74d6b4124
dextermallo <dextermallo@gmail.com>
2025-01-14 14:23:45 +0800

    chore: move test case

--- script/utils/_log.sh
+++ script/utils/_log.sh
@@ -111,3 +111,38 @@ function _logger() {
 
     _check_logger_level ${arg_instruction:-$arg_level} && _log -f $level_color $arg_no_newline $arg_bold "$mark_msg$arg_message" 
 }
+
+function _test.logger() {
+    _swiss_logger_level="warn"
+    echo "=== current level: $_swiss_logger_level ==="
+    _logger -l error -n "new line test: " && _logger -l warn --no-mark "merging color"
+    _logger -l debug -i "this should bypass logger_level restriction"
+    
+    _swiss_logger_level="debug"
+    echo "=== current level: $_swiss_logger_level ==="
+
+    _logger -l debug "test debug"
+    _logger -l info "test info"
+    _logger -l hint "test hint"
+    _logger -l warn "test warn"
+    _logger -l error "test error"
+    _logger -l important "test important"
+
+    _swiss_logger_level="error"
+    echo "=== current level: $_swiss_logger_level ==="
+    _logger -l debug "test debug"
+    _logger -l info "test info"
+    _logger -l hint "test hint"
+    _logger -l warn "test warn"
+    _logger -l error "test error"
+    _logger -l important "test important"
+
+    _swiss_logger_level="warn"
+    echo "=== current level: $_swiss_logger_level ==="
+    _logger -l debug "test debug"
+    _logger -l info "test info"
+    _logger -l hint "test hint"
+    _logger -l warn "test warn"
+    _logger -l error "test error"
+    _logger -l important "test important"
+}
